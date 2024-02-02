7439f8316a85d813f5e9d6400ba014664a02ae9d
Peter Rhode <peter@air-von-peter.speedport.ip>
2024-02-02 15:06:52 +0100

    add data file

---
+++ src/data/questions.js
@@ -0,0 +1,37 @@
+export const questions = [
+  {
+    question: "Wie selektiert man ein Element mit id=demo",
+    answers: ["#demo", ".demo", "Demo", "*demo"],
+    correctIndex: 0,
+  },
+  {
+    question: "Wofür steht CSS?",
+    answers: [
+      "Computer Style Sheets",
+      "Cascading Style Sheets",
+      "Colored Style Sheets",
+      "Creative Style Sheets",
+    ],
+    correctIndex: 1,
+  },
+  {
+    question: "Welche CSS Syntax ist korrekt?",
+    answers: [
+      "{body: color=black;}",
+      "{body; color: black;}",
+      "body {color: black;}",
+      "body: color=black;",
+    ],
+    correctIndex: 2,
+  },
+  {
+    question: "Was ist der Defaultwert für position?",
+    answers: ["relative", "fixed", "absolute", "static"],
+    correctIndex: 3,
+  },
+  {
+    question: "Wie heißt Ralf mit zweitem Vornamen?",
+    answers: ["Thomas", "Ulrich", "Sabine", "Jürgen"],
+    correctIndex: 3,
+  },
+];
