b201af73729ca2099d2857bf1a09b030328d7eec
Falk König <fk@falkkoenig.de>
2024-02-05 11:22:38 +0100

    add questions logic

--- src/components/GameScreen.jsx
+++ src/components/GameScreen.jsx
@@ -1,19 +1,70 @@
+import { useState } from "react";
 import styles from "./GameScreen.module.css";
 import { ProgressBar } from "./ProgressBar";
-import { Questions } from "./Questions";
 
-export function GameScreen({ onEndQuiz }) {
+import { questions } from "../assets/data/questions";
+
+export function GameScreen({
+  onEndQuiz,
+  onShowResultScreen,
+  onGetAmountCorrectAnswers,
+}) {
+  const [round, setRound] = useState(0);
+  const [activeId, setActiveId] = useState(null);
+
+  function handleNextRound() {
+    if (round < 4) {
+      setRound((prevRound) => prevRound + 1);
+    }
+    setActiveId((prevActiveId) => (prevActiveId = null));
+  }
+
   return (
     <>
-      <ProgressBar />
+      <ProgressBar round={round} amountQuestion={questions.length} />
       <div className={styles["game-field"]}>
+        <h2>{questions[round].question}</h2>
         <ul>
-          <Questions />
+          {questions[round].answers.map((answer, index) => {
+            return (
+              <li
+                key={index}
+                onClick={() => {
+                  if (activeId === null) {
+                    setActiveId((prevActiveId) => (prevActiveId = index));
+                    if (questions[round].correctIndex === index) {
+                      onGetAmountCorrectAnswers();
+                    }
+                  }
+                }}
+                className={`
+                  ${styles["answer-listElement"]} 
+                  ${
+                    questions[round].correctIndex === index &&
+                    activeId !== null
+                      ? styles["answer-true"]
+                      : ""
+                  }
+                  ${
+                    questions[round].correctIndex !== index &&
+                    activeId !== null &&
+                    activeId === index
+                      ? styles["answer-false"]
+                      : ""
+                  }
+                  `}
+              >
+                {answer}
+              </li>
+            );
+          })}
         </ul>
       </div>
 
       <div className={styles["button-bar"]}>
-        <button>WEITER</button>
+        <button onClick={round < 4 ? handleNextRound : onShowResultScreen}>
+          {round < 4 ? "WEITER" : "ZUM ERGEBNIS"}
+        </button>
         <button onClick={onEndQuiz}>CANCEL</button>
       </div>
     </>
--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -1,20 +1,48 @@
 import styles from "./Questions.module.css";
 import { questions } from "../assets/data/questions";
+import { useState } from "react";
 
-export function Questions() {
+export function Questions({ round, onGetAmountCorrectAnswers }) {
+  const [activeId, setActiveId] = useState(null);
+
   return (
     <>
-      <h2>{questions[2].question}</h2>
+      <h2>{questions[round].question}</h2>
       <ul>
         {questions.map((question, index) => {
-          if (index === 2) {
-            return question.answers.map((answer, idx) => {
+          if (index === round) {
+            return question.answers.map((answer, index) => {
               return (
                 <li
-                  key={idx}
-                  className={`${styles["answer-listElement"]}`}
+                  key={index}
+                  id={index}
+                  onClick={() => {
+                    if (activeId === null) {
+                      setActiveId(
+                        (prevActiveId) => (prevActiveId = index)
+                      );
+                      if (question.correctIndex === index) {
+                        onGetAmountCorrectAnswers();
+                      }
+                    }
+                  }}
+                  className={`
+                  ${styles["answer-listElement"]} 
+                  ${
+                    question.correctIndex === index && activeId !== null
+                      ? styles["answer-true"]
+                      : ""
+                  }
+                  ${
+                    question.correctIndex !== index &&
+                    activeId !== null &&
+                    activeId === index
+                      ? styles["answer-false"]
+                      : ""
+                  }
+                  `}
                 >
-                  <a>{answer} </a>
+                  {answer}
                 </li>
               );
             });
