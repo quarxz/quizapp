8a7f12886695db38c21f9eda240cc121c30c7566
Falk König <fk@falkkoenig.de>
2024-02-06 15:25:31 +0100

    add sound to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,16 @@ import { GameScreen } from "./components/GameScreen";
 import { ResultScreen } from "./components/ResultScreen";
 import { AdvertisingScreen } from "./components/AdvertisingScreen";
 
+import bell from "./assets/sounds/bell.wav";
+import nope from "./assets/sounds/nope.wav";
+
 function App() {
   const [screen, setScreen] = useState("welcome");
   const [amountCorrectAnswers, setAmountCorrectAnswers] = useState(0);
+
+  const [mute, toggleMute] = useState(false);
+  const bellSound = new Audio(bell);
+  const nopeSound = new Audio(nope);
 
   return (
     <div className={styles.board}>
@@ -44,6 +51,14 @@ function App() {
             setAmountCorrectAnswers(
               (prevAmountCorrectAnswers) => prevAmountCorrectAnswers + 1
             );
+          }}
+          onPlaySound={(option) => {
+            if (option) {
+              !mute && bellSound.play();
+            }
+            if (!option) {
+              !mute && nopeSound.play();
+            }
           }}
         />
       ) : undefined}
